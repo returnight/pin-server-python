d60131a6a794a05c1adce6d075b7e3d3be26109b
G_will <g_will@ieqi.com>
2012-09-13 15:42:42 +0800

    fix bug

--- pinserver/models/comment.py
+++ pinserver/models/comment.py
@@ -14,7 +14,7 @@ from pinserver.extensions import db
 from pinserver.models.user import User
 from pinserver.models.pin import Pin
 
-class Comment():
+class Comment(db.Document):
     """
         Comment
         ~~~~~~~~
