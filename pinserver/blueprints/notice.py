40f4130a4b562e00fe68f2c49325c595e8e17df0
G_will <g_will@ieqi.com>
2013-01-31 11:47:30 +0800

    fix notices

--- pinserver/blueprints/notice.py
+++ pinserver/blueprints/notice.py
@@ -52,7 +52,7 @@ def show_notices(page_num):
         limit = 5
         start = (page_num - 1) * limit
         end = page_num * limit
-        notices = Notice.objects(readed=False)[start,end].order_by('-create_at')
+        notices = Notice.objects(readed=False)[start:end].order_by('-create_at')
         res_data = notices_pack(notices)
         return (json.dumps(res_data), 200)
     return ('show notices session timeout', 400)
